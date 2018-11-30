2eb46093f2d4980f126482073214b81dbf697326
juanfpo96 <juanfpo96@gmail.com>
2018-11-30 12:00:59 +0100

    Updated server port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const hostname = '127.0.0.1';
-const port = 3000;
+const port =  process.env.PORT || 3000;
 const server = require('./controller.js');
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
